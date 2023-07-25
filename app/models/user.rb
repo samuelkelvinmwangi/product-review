788af6051ae53973259f383b2a8a8b42dc581df9
samuel <samuelkelvinmwangi@gmail.com>
2023-07-26 02:10:55 +0300

    finishing up on usera rb file for favorite products and removing reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,14 @@ class User < ActiveRecord::Base
     def products
         self.products
     end
+
+    # favorite products
+    def favorite_product
+        reviews.order(star_rating: :desc).first.product
+    end
+
+    # remove reviews
+    def remove_reviews(product)
+        reviews.where(product: product).destroy_all
+    end
 end
