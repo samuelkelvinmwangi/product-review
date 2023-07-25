082ed12007d882a3d3934c6bfb7a4a075dcc7364
samuel <samuelkelvinmwangi@gmail.com>
2023-07-26 01:23:13 +0300

    adding associations of the review to user and product in the reviews rb file

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,14 @@
 class CreateReview < ActiveRecord::Base
+    belongs_to :user
+    belongs_to :product
+
+    # association of review and user
+    def user
+        self.user
+    end
     
+    # association of review and product
+    def product
+        self.product
+    end
 end
