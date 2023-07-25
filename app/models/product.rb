6043d4f195d4a2512934d3c9b3000233acb53f71
samuel <samuelkelvinmwangi@gmail.com>
2023-07-26 02:08:36 +0300

    finishing on product rb file for review leaving, printing all reviews and rating average

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,5 +11,25 @@ class Product < ActiveRecord::Base
     def users
         self.users
     end
+
+    #leave_review(user, star_rating, comment)
+    def leave_review(user, star_rating, comment)
+    Review.create(user: user, product: self, star_rating: star_rating, comment: comment)
+    end
+
+    #print_all_reviews
+    def print_all_reviews
+        reviews.each do |review|
+            puts "Review for #{name} by #{review.user.name}: #{review.star_rating}. #{review.comment}"
+         end
+    end
+    
+    # average_rating
+    def average_rating
+        total_ratings = reviews.sum(:star_rating)
+        total_reviews = reviews.count
+
+        total_reviews > 0 ? total_ratings.to_f / total_reviews : 0
+    end
     
 end
