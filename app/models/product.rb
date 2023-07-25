3d534b9b69243c1c421e338622432c1f564af93b
samuel <samuelkelvinmwangi@gmail.com>
2023-07-26 01:12:07 +0300

    creating associations of product with reviews and users

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,15 @@
 class Product < ActiveRecord::Base
+    has_many :reviews
+    has_many :users, through: :reviews
+   
+    # creating associations of product and  reviews
+    def reviews
+        self.reviews
+    end
+
+    # creating associations of product and users
+    def users
+        self.users
+    end
     
 end
