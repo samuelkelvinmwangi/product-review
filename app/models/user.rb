48f6577869f5f881d73060958c83981d8b42009f
samuel <samuelkelvinmwangi@gmail.com>
2023-07-26 01:16:33 +0300

    making associations of user with reviews and products in the user rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ActiveRecord::Base
+    has_many :reviews
+    has_many :products, through: :reviews
+
+    # association with reviews
+    def reviews
+        self.reviews
+    end
     
+    # association with products
+    def products
+        self.products
+    end
 end
